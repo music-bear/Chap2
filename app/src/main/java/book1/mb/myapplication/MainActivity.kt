14607bfd56b27667a2f711ea2b50fc658c72338b
music-bear <haikinngubear@yahoo.co.jp>
2019-10-20 12:30:28 +0900

    初回コミット

---
+++ app/src/main/java/book1/mb/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package book1.mb.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        morningBtn.setOnClickListener{
+            greetingText.text = getString(R.string.good_morning_text)
+        }
+
+
+        afternoonBtn.setOnClickListener{
+            greetingText.text = getString(R.string.good_afternoon_text)
+        }
+
+
+        eveningBtn.setOnClickListener{
+            greetingText.text = getString(R.string.good_evening_text)
+        }
+
+    }
+}
